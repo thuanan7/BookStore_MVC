d66c82890df9518c225c8a47db216fb751f6ac8c
thuanan <thuanan7@gmail.com>
2023-07-09 23:33:40 +0700

    Display all Categories, Bootswatch Theme and Bootstrap Icons

--- BookWeb/Controllers/CategoryController.cs
+++ BookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookWeb.Data;
+using BookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> categoryList = _context.Categories.ToList();
+            return View(categoryList);
         }
     }
 }
