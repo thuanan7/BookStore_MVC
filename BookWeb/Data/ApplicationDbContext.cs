ec354bda9f2b2f152c03a2c479efa508b6de8998
thuanan <thuanan7@gmail.com>
2023-07-09 16:12:41 +0700

    setting ConnectionString and config DbContext entity framework

---
+++ BookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
