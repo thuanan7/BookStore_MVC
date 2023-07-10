7308066a50816e68b552734500c4f5d7cde338e8
thuanan <thuanan7@gmail.com>
2023-07-10 22:07:11 +0700

    edit and delete category

--- BookWeb/Controllers/CategoryController.cs
+++ BookWeb/Controllers/CategoryController.cs
@@ -35,5 +35,49 @@ namespace BookWeb.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id <= 0)
+            {
+                return NotFound();
+            }
+            Category? category = _context.Categories.Find(id);
+            //Category? category2 = _context.Categories.FirstOrDefault(c => c.Id == id);
+            //Category? category3 = _context.Categories.Where(c => c.Id == id).FirstOrDefault();
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirm(int? id)
+        {
+            if (id == null || id <= 0)
+            {
+                return NotFound();
+            }
+            Category? category = _context.Categories.Find(id);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Delete(int? id)
+        {
+            Category? category = _context.Categories.Find(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return NotFound();
+        }
     }
 }
