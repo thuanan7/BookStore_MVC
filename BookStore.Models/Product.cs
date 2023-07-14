19cf3758d3a0f5932fa26d8a00e21eb7cf15ba03
thuanan <thuanan7@gmail.com>
2023-07-14 21:37:52 +0700

    Product CRUD

--- BookStore.Models/Product.cs
+++ BookStore.Models/Product.cs
@@ -5,6 +5,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography.X509Certificates;
 
 namespace BookStore.Models
 {
@@ -38,5 +40,11 @@ namespace BookStore.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
--- BookStoreWeb/Areas/Admin/Controllers/ProductController.cs
+++ BookStoreWeb/Areas/Admin/Controllers/ProductController.cs
@@ -17,5 +17,70 @@ namespace BookStoreWeb.Areas.Admin.Controllers
             List<Product> products = _unitOfWork.ProductRepository.GetAll().ToList();
             return View(products);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.ProductRepository.Add(product);
+                _unitOfWork.Save();
+                TempData["success"] = "New Book has been created!";
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id <= 0)
+            {
+                return NotFound();
+            }
+            Product product = _unitOfWork.ProductRepository.Get(p=>p.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.ProductRepository.Update(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Book has been updated!";
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirm(int? id)
+        {
+            if (id == null || id <= 0)
+            {
+                return NotFound();
+            }
+            Product product = _unitOfWork.ProductRepository.Get(p => p.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int? id)
+        {
+            Product? product = _unitOfWork.ProductRepository.Get(p => p.Id == id);
+            if (product != null)
+            {
+                _unitOfWork.ProductRepository.Remove(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Book has been deleted!";
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
     }
 }
