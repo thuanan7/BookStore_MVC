1703b12865c43654e1600350a77f5cc6821600e6
thuanan <thuanan7@gmail.com>
2023-07-17 15:27:53 +0700

    Combine, Create and Edit Pages

--- BookStoreWeb/Areas/Admin/Controllers/ProductController.cs
+++ BookStoreWeb/Areas/Admin/Controllers/ProductController.cs
@@ -20,38 +20,47 @@ namespace BookStoreWeb.Areas.Admin.Controllers
             return View(products);
         }
 
-        public IActionResult Create()
+        //Update(edit) + Insert(create) = Upsert
+        public IActionResult Upsert(int? id)
         {
-            IEnumerable<SelectListItem> CategorySelectList = _unitOfWork.CategoryRepository.GetAll()
+            ProductVM productVM = new ProductVM()
+            {
+                CategorySelectList = _unitOfWork.CategoryRepository.GetAll()
                 .Select(o => new SelectListItem
                 {
                     Text = o.Name,
                     Value = o.Id.ToString()
-                });
-
-            //ViewBag will internally insert data with the key to ViewDataDictionary so must not set ViewData and ViewBag have the same key.
-            //ViewBag.CategorySelectList = CategorySelectList;
-            //ViewData["CategorySelectList"] = CategorySelectList;
-
-            //Avoid using ViewBag and ViewData
-            //ViewModel
-            ProductVM productVM = new ProductVM()
-            {
-                CategorySelectList = CategorySelectList,
+                }),
                 Product = new Product()
             };
+            if (id != null && id != 0)
+            {
+                productVM.Product = _unitOfWork.ProductRepository.Get(u => u.Id == id);
+            }
             return View(productVM);
         }
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.ProductRepository.Add(productVM.Product);
-                _unitOfWork.Save();
-                TempData["success"] = "New Book has been created!";
-                return RedirectToAction("Index", "Product");
+                if (productVM.Product.Id == 0)
+                {
+                    //Create
+                    _unitOfWork.ProductRepository.Add(productVM.Product);
+                    _unitOfWork.Save();
+                    TempData["success"] = "New Book has been created!";
+                    return RedirectToAction("Index", "Product");
+                }
+                else
+                {
+                    //Update
+                    _unitOfWork.ProductRepository.Update(productVM.Product);
+                    _unitOfWork.Save();
+                    TempData["success"] = "Book has been updated!";
+                    return RedirectToAction("Index", "Product");
+                }
             }
             productVM.CategorySelectList = _unitOfWork.CategoryRepository.GetAll()
                 .Select(o => new SelectListItem
@@ -59,31 +68,10 @@ namespace BookStoreWeb.Areas.Admin.Controllers
                     Text = o.Name,
                     Value = o.Id.ToString()
                 });
+
             return View(productVM);
         }
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id <= 0)
-            {
-                return NotFound();
-            }
-            Product product = _unitOfWork.ProductRepository.Get(p => p.Id == id);
-            return View(product);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.ProductRepository.Update(product);
-                _unitOfWork.Save();
-                TempData["success"] = "Book has been updated!";
-                return RedirectToAction("Index", "Product");
-            }
-            return View();
-        }
         [ActionName("Delete")]
         public IActionResult DeleteConfirm(int? id)
         {
