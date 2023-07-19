0ee25e3b034bf2669855e4de291bb399aa952ac5
thuanan <thuanan7@gmail.com>
2023-07-19 17:07:00 +0700

    Handle Image on Update and Create Product

--- BookStoreWeb/Areas/Admin/Controllers/ProductController.cs
+++ BookStoreWeb/Areas/Admin/Controllers/ProductController.cs
@@ -3,6 +3,8 @@ using BookStore.Models;
 using BookStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BookStoreWeb.Areas.Admin.Controllers
 {
@@ -10,9 +12,11 @@ namespace BookStoreWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -45,22 +49,41 @@ namespace BookStoreWeb.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                if (file != null)
+                {
+                    string wwwRootPath = _webHostEnvironment.WebRootPath;
+                    string path = Path.Combine(wwwRootPath, @"images\product");
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                    if (!String.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        string oldFilePath = Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+                        if(System.IO.File.Exists(oldFilePath))
+                        {
+                            System.IO.File.Delete(oldFilePath);
+                        }
+                    }
+                    
+
+                    using (var stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
                 if (productVM.Product.Id == 0)
                 {
-                    //Create
                     _unitOfWork.ProductRepository.Add(productVM.Product);
-                    _unitOfWork.Save();
-                    TempData["success"] = "New Book has been created!";
-                    return RedirectToAction("Index", "Product");
                 }
                 else
                 {
-                    //Update
                     _unitOfWork.ProductRepository.Update(productVM.Product);
-                    _unitOfWork.Save();
-                    TempData["success"] = "Book has been updated!";
-                    return RedirectToAction("Index", "Product");
                 }
+                
+                _unitOfWork.Save();
+                TempData["success"] = "New Book has been created!";
+
+                return RedirectToAction("Index", "Product");
             }
             productVM.CategorySelectList = _unitOfWork.CategoryRepository.GetAll()
                 .Select(o => new SelectListItem
