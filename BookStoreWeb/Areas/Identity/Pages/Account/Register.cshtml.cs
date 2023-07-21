58a1a955c4ade22bf08a3cba07d74bcf3a98b08b
thuanan <thuanan7@gmail.com>
2023-07-21 15:06:11 +0700

    Register Other Fields

--- BookStoreWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookStoreWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -107,6 +107,14 @@ namespace BookStoreWeb.Areas.Identity.Pages.Account
             public string? Role { get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleSelectList { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
 
@@ -141,6 +149,12 @@ namespace BookStoreWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Name = Input.Name;
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
