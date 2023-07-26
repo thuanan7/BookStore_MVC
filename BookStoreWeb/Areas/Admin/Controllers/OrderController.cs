18a31e6e887082723444261576ef239cbb1015e3
thuanan <thuanan7@gmail.com>
2023-07-26 19:35:52 +0700

    OrderVM and Order Controller

---
+++ BookStoreWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWeb.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALL
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> orderHeaders = _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = orderHeaders });
+		}
+
+		#endregion
+	}
+}
