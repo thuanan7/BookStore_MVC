debb067ad26b57430a0b5d87d20a15e4459f0479
thuanan7 <thuanan7@gmail.com>
2024-03-28 12:52:32 +0700

    update connectionstring

--- BookStore.DataAccess/Migrations/20230714084302_addImageUrlToProduct.cs
+++ BookStore.DataAccess/Migrations/20230714084302_addImageUrlToProduct.cs
@@ -15,49 +15,49 @@ namespace BookStore.DataAccess.Migrations
                 table: "Products",
                 type: "nvarchar(max)",
                 nullable: false,
-                defaultValue: "");
+                defaultValue: "https://placehold.co/600x400");
 
             migrationBuilder.UpdateData(
                 table: "Products",
                 keyColumn: "Id",
                 keyValue: 1,
                 column: "ImageUrl",
-                value: "");
+                value: "https://placehold.co/600x401");
 
             migrationBuilder.UpdateData(
                 table: "Products",
                 keyColumn: "Id",
                 keyValue: 2,
                 column: "ImageUrl",
-                value: "");
+                value: "https://placehold.co/600x402");
 
             migrationBuilder.UpdateData(
                 table: "Products",
                 keyColumn: "Id",
                 keyValue: 3,
                 column: "ImageUrl",
-                value: "");
+                value: "https://placehold.co/600x403");
 
             migrationBuilder.UpdateData(
                 table: "Products",
                 keyColumn: "Id",
                 keyValue: 4,
                 column: "ImageUrl",
-                value: "");
+                value: "https://placehold.co/600x404");
 
             migrationBuilder.UpdateData(
                 table: "Products",
                 keyColumn: "Id",
                 keyValue: 5,
                 column: "ImageUrl",
-                value: "");
+                value: "https://placehold.co/600x405");
 
             migrationBuilder.UpdateData(
                 table: "Products",
                 keyColumn: "Id",
                 keyValue: 6,
                 column: "ImageUrl",
-                value: "");
+                value: "https://placehold.co/600x406");
         }
 
         /// <inheritdoc />
